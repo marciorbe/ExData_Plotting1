c905a79505e2aa0d0a5407b02b7e240946f931c1
marciorbe <marciorbe@gmail.com>
2014-12-07 18:20:46 -0300

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -28,6 +28,6 @@ with(work, points( strptime( paste(as.character(work[,"Date"]),work[,"Time"]) , 
                    Sub_metering_3,col="blue",
 				   type = "l") )
 				   
-legend("topright", pch = "-", col = c("black", "red","blue"), legend = c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"))
+legend("topright", lty=1, col = c("black", "red","blue"), legend = c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"))
 
 dev.off()
